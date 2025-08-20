1dd31afa791f4a7f0f04ef1db7c1e222213709ae
safinelahi <safinelahi@gmail.com>
2025-08-20 23:52:31 +0600

    update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,40 +1,7 @@
-import { FiSearch } from "react-icons/fi";
-import logoImage from "../../assets/icons/জমি জগ 1.jpg";
 import heroImage from "../../assets/images/hero page.jpg";
 const Home = () => {
   return (
     <div>
-      {/* nav bar section */}
-      <div className=" flex items-center justify-between pl-[8%] pr-[8%] mt-9 ">
-        {/* logo section */}
-        <div>
-          <img className=" items-center " src={logoImage} alt="" />
-        </div>
-
-        {/* nav items section */}
-        <div className=" flex items-center gap-7  text-[#151515] text-xl font-medium ">
-          <a href="">হোম</a>
-          <a href="">সার্ভেয়ার</a>
-          <a href="">আমাদের সম্পর্কে</a>
-          <a href="">জ্ঞাতব্য</a>
-        </div>
-
-        {/* search section */}
-        <div className="flex gap-6">
-          <div className="flex items-center w-64 bg-[#f5f5eb] rounded-md px-3 py-2">
-            <FiSearch className="text-gray-500 mr-2" />
-            <input
-              type="text"
-              placeholder="সার্চ করুন"
-              className="bg-transparent focus:outline-none text-gray-700 w-full"
-            />
-          </div>
-          <button className=" text-white font-semibold text-lg bg-[#7ED957] px-[28px] py-[14px] rounded-lg ">
-            লগইন
-          </button>
-        </div>
-      </div>
-
       {/* hero section image  */}
 
       <div>
@@ -69,11 +36,8 @@ const Home = () => {
           দিনাজপুর
         </button>
       </div>
-      
 
       {/* Footer section */}
-
-
     </div>
   );
 };
