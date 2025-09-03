7b4cd4afdfb0ee5a2b9abcfa3bcd6465e132f350
alfazsozib <alfazsozib@gmail.com>
2025-09-03 06:08:15 +0600

    signup -v2

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      tls: true, // force TLS
+      tls: true,
     });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
